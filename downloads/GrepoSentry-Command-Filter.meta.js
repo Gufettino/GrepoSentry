12f61c00452044b872318c224dff8fb4d93e15c6
Gufettino <101306577+Biscottino42@users.noreply.github.com>
2026-04-25 00:45:15 +0200

    Add files via upload

---
+++ downloads/GrepoSentry-Command-Filter.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         GrepoSentry - Command Filter
+// @namespace    https://greasyfork.org/
+// @version      1.0.4
+// @description  Advanced command filter for Grepolis - Created by Gufettino | SilthersGaming.net
+// @author       Gufettino (SilthersGaming.net)
+// @include      http://*.grepolis.com/game/*
+// @include      https://*.grepolis.com/game/*
+// @exclude      view-source://*
+// @match        https://*.grepolis.com/game/*
+// @grant        GM_addStyle
+// @run-at       document-idle
+// @homepageURL  https://grepolis.latavernadeglisbronzi.net/script.php
+// @supportURL   https://grepolis.latavernadeglisbronzi.net/script.php
+// @downloadURL  https://grepolis.latavernadeglisbronzi.net/downloads/GrepoSentry-Command-Filter.user.js
+// @updateURL    https://grepolis.latavernadeglisbronzi.net/downloads/GrepoSentry-Command-Filter.meta.js
+// ==/UserScript==
